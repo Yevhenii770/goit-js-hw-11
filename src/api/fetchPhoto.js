e4e263d694a30ed81d556a20533449e0a9472aea
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-01-30 03:13:19 +0100

    created styles + hbs

---
+++ src/api/fetchPhoto.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const KEY = '33191219-dc41095899386e0adcb39eb2c';
+
+export default class SearchPhotoApiService {
+    constructor() {
+        this.serchQuery = '';
+        this.page = 1;
+    }
+
+   async fetchchPhoto() {
+        const url = `https://pixabay.com/api/?key=${KEY}&q=${this.serchQuery}&page=${this.page}&image_type=photo&${this.page}&per_page=40&orientation=horizontal&safesearch=true`;
+         return fetch(url)
+            .then(r => r.json())
+            .then(data => {
+                this.incrementPage()
+                return data.hits
+            });
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+    resetPage() {
+        this.page = 1;
+    }
+    get query() {
+        return this.serchQuery
+    }
+    set query(newQuery) {
+        this.serchQuery = newQuery;    
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,83 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SearchPhotoApiService from './api/fetchPhoto.js'
 import photoTpl from './templates/photo-tpl.hbs'
-const debounce = require('lodash.debounce');
+
 
 
 const refs = {
-  form: document.querySelector('#search-form') , 
-  btnSub: document.querySelector('button'),
+  searchForm: document.querySelector('.search-form'),
+  container: document.querySelector('.gallery'),
+  btnMore: document.querySelector('.load-more'),
+  gallery: document.querySelector('.gallery'),
 }
+
+const searchPhotoApi = new SearchPhotoApiService();
+
+
+refs.searchForm.addEventListener('submit', onSearch)
+refs.btnMore.addEventListener('click', onLoadMore)
+// refs.gallery.addEventListener('click', onGallaryClick);
+
+
+// function onGallaryClick() {
+//   new SimpleLightbox('.gallery a')
+// }
+
+
+function onSearch(e) {
+  e.preventDefault()
+  refs.btnMore.removeAttribute("disabled")
+
+   if (!e.target.searchQuery.value.trim()) {
+    return;
+  } 
+  
+  searchPhotoApi.query = e.currentTarget.elements.searchQuery.value.trim();
+  refs.btnMore.classList.remove('is-hidden');
+  onLoadMore()
+  e.target.reset()
+  clearContainer();
+}
+
+
+
+
+
+
+function makeMarkup(hits) {
+  for (const element of hits) {
+     refs.container.insertAdjacentHTML('beforeend', photoTpl(element))
+  }
+}
+
+function clearContainer() {
+  refs.container.innerHTML = ''
+}
+
+function onLoadMore() {
+
+ searchPhotoApi.fetchchPhoto().then(hits => {
+    if (hits.length == 0) {
+      return Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+    }
+    makeMarkup(hits);
+ });
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
